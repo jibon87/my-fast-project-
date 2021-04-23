57fe95b3b6de90940970587c0a65ffa9cb3e629e
jibon87 <3djibon1@gmail.com>
2021-04-24 03:08:29 +0600

    half work finished

---
+++ app.js
@@ -0,0 +1,21 @@
+
+(function ($) {
+    $(document).ready(function () {
+
+         $('#tog').click(function () {
+             $('.nav').slideToggle(1000);
+         })
+
+
+
+
+
+
+
+
+
+
+         
+
+    })
+})(jQuery)
