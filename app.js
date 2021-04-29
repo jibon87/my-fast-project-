c70b8fd301d03b4fe69ee58c085f293fd1ca1882
jibon87 <3djibon1@gmail.com>
2021-04-30 01:41:49 +0600

    scroll bar add

--- app.js
+++ app.js
@@ -7,6 +7,24 @@
          })
 
 
+         // for to-top buttom 
+         $('.to-top a').click(function () {
+             $('html, body').animate({
+                 'scrollTop': 0
+             }, 2000);
+             return false;
+         })
+
+         // for to-top button heid and show
+         $(window).scroll(function () {
+             if ($(window).scrollTop() > 500) {
+                 $('.to-top a').show();
+             } else {
+                 $('.to-top a').hide();
+             }
+         })
+
+
 
 
 
